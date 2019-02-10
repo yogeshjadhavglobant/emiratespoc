a0da0eef856c48a869f3c2d6198047ca37550e60
Yogesh Jadhav <yogesh.jadhav@globant.com>
2019-02-10 12:51:52 +0530

    changes to pom file and dockerfile added

--- emirates-microservice/src/main/java/com/emirates/couchbase/entity/Flight.java
+++ emirates-microservice/src/main/java/com/emirates/couchbase/entity/Flight.java
@@ -35,4 +35,30 @@ public class Flight {
 	@NotNull
     @Field
 	private String type;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
